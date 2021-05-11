c5d808becd71cdc80e070a121cff27313ba06a4d
M Khoirul Rizal <khoirul@mggosftware.com>
2021-05-12 00:10:26 +0700

    feat: add employee

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ import Edits from './pages/Edits.vue'
 Vue.use(VueRouter)
 Vue.use(Vuex)
 
+// axios
+axios.defaults.baseURL =  'https://jsonplaceholder.typicode.com'
+
 // Router
 const routes = [
 	{path: '/home', component: Home},
@@ -41,16 +44,28 @@ const store = new Vuex.Store({
     GetEmployees () {
       return new Promise ((resolve, reject) => {
         console.log('GetEmployees main.js 0')
-        axios({
-          url: '/users',
-          baseURL: 'https://jsonplaceholder.typicode.com'
-        })
+        axios.get('/users')
           .then( resp => {
             console.log('GetEmployees main.js 1T', resp)
             resolve(resp.data)
           })
           .catch( err => {
             console.log('GetEmployees main.js 1F', err)
+            reject(err)
+          })
+      })
+    },
+    AddEmployees ({state}, employee) {
+      console.log('AddEmployees -1', state)
+      console.log('AddEmployees 0', employee)
+      return new Promise ((resolve, reject) => {
+        axios.post('/users', employee)
+          .then( resp => {
+            console.log('AddEmployees 1T', resp)
+            resolve(resp.data)
+          })
+          .catch( err => {
+            console.log('AddEmployees 1F', err)
             reject(err)
           })
       })
--- src/pages/tentang.vue
+++ src/pages/tentang.vue
@@ -3,8 +3,12 @@
 		pages/About
     <button v-on:click="GetEmployees"> Get Employyes </button>
     <div class="employee-container">
+      <div class="employee-inf">
+        <input class="employee-textfield" v-model="name" placeholder="Add Employees" />
+        <input type="submit" label="add" v-on:click="AddEmployee" />
+      </div>
       <div class="employee-inf" v-for="employee in employees" v-bind:key="employee.id"> 
-        {{ employee.name }}
+        {{ employee.id }} || {{ employee.name }}
       </div>
     </div>
 	</div>
@@ -15,10 +19,27 @@
 		name : 'home',
     data () {
       return {
-        employees: []
+        employees: [],
+        name: null
       }
     },
+    created() {
+      this.GetEmployees()
+    },
     methods: {
+      async AddEmployee () {
+        try {
+          const params = {
+            name: this.name
+          }
+          const employee = await this.$store.dispatch('AddEmployees', params)
+          this.employees.push(employee)
+        } catch (err) {
+          console.log(err)
+        }
+
+        this.name = null
+      },
       async GetEmployees () {
         console.log('GetEmployees tentang 0', this.employees)
         try {
@@ -50,6 +71,11 @@
 }
 #tentang .employee-inf {
   border-bottom: solid 1px black;
-  padding: 5px 10px
+  padding: 5px 10px;
+  display: flex;
+  justify-content: space-between
+}
+#tentang .employee-inf .employee-textfield {
+  width: 90%
 }
 </style>
